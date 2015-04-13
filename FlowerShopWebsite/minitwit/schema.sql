2f9c490c466c0507c2ebfbbd22e436251ce7977c
Lexi Christiansen <lchristiansen@izeni.com>
2015-04-12 20:39:28 -0600

    Basis to modify later

---
+++ FlowerShopWebsite/minitwit/schema.sql
@@ -0,0 +1,25 @@
+
+create table user (
+  user_id integer primary key autoincrement,
+  username text not null,
+  email text not null,
+  pw_hash text not null
+);
+
+create table follower (
+  who_id integer,
+  whom_id integer
+);
+
+create table message (
+  message_id integer primary key autoincrement,
+  author_id integer not null,
+  text text not null,
+  pub_date integer
+);
+
+
+create table access_token (
+  user_id integer not null,
+  access_token_text text not null
+);
